db775c1dc02851904db0cdec9a1f8af21a2e21ae
vinanolin <vinanolin@gmail.com>
2022-04-08 17:24:37 -0300

    master

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Painel')
+
+@section('content_header')
+    <h1>Painel</h1>
+@stop
+
+@section('content')
+    <p>Conteúdo do meu projeto</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
