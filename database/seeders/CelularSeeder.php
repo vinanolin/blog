e84150abf6925bf3ec8dee5d1656b225e29bf382
vinanolin <vinanolin@gmail.com>
2022-04-13 19:18:03 -0300

    RemoveDescricao

--- database/seeders/CelularSeeder.php
+++ database/seeders/CelularSeeder.php
@@ -17,19 +17,19 @@ class CelularSeeder extends Seeder
         Celular::create([
             'nome'      => 'XIAOMI - MI 8 128GB',
             'valor'     => '2000',
-            'descricao' => 'Celular chinês de gente esquisita'
+    
         ]);
 
         Celular::create([
             'nome'      => 'XIAOMI - MI 8 64GB',
             'valor'     => '1500',
-            'descricao' => 'Celular chinês de gente esquisita e pobre'
+
         ]);
 
         Celular::create([
             'nome'      => 'XIAOMI - MI 8 256GB',
             'valor'     => '2500',
-            'descricao' => 'Celular chinês de gente esquisita e rica'
+
         ]);
 
         //
